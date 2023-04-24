21559fb386dde8a60a66e52b08d131cd44207e59
Abdi Berhe <abdi201380@gmail.com>
2023-04-25 00:20:29 +0300

    checking for empty array and single sized array and correcting index range

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,7 +12,10 @@ void selection_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
-	for (i = 0; i < size; i++)
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
 		int smallestValue = array[i];
 		size_t smallestIndex = i;
